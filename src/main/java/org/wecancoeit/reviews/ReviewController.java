41793ac6c16a14c368bf35746cffa0d0aa1739e5
JamieCBarr <jamiecbarr@gmail.com>
2022-06-05 08:55:05 -0400

    C:/Program Files/Git/reviewsTemplate can now findAll() from ReviewRepository

--- src/main/java/org/wecancoeit/reviews/ReviewController.java
+++ src/main/java/org/wecancoeit/reviews/ReviewController.java
@@ -1,13 +1,20 @@
 package org.wecancoeit.reviews;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
 
 @Controller
 public class ReviewController {
 
+    @Resource
+    private ReviewRepository reviewRepo;
+
     @RequestMapping("/reviews")
-    public String findAllReviews(){
+    public String findAllReviews(Model model){
+        model.addAttribute("reviewsModel", reviewRepo.findAll());
         return "reviewsTemplate";
     }
 }
--- src/test/java/org/wecancoeit/reviews/ReviewControllerMvcTest.java
+++ src/test/java/org/wecancoeit/reviews/ReviewControllerMvcTest.java
@@ -2,11 +2,16 @@ package org.wecancoeit.reviews;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.mockito.Mock;
+import static org.mockito.Mockito.when;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 @WebMvcTest(ReviewController.class)
 public class ReviewControllerMvcTest {
@@ -14,10 +19,27 @@ public class ReviewControllerMvcTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private ReviewRepository reviewRepo;
+
+    @Mock
+    private Review review1;
+
+    @Mock
+    private Review review2;
+
+
+
     @Test
     public void shouldBeOkForAllReviewsInTheReviewsTemplate() throws Exception{
         mockMvc.perform(get("/reviews")).andExpect(status().isOk())
                 .andExpect(view().name("reviewsTemplate"));
     }
 
+    @Test
+    public void shouldFindAllReviewsInModel() throws Exception{
+        Collection<Review> allReviewsInModel = Arrays.asList(review1, review2);
+        when(reviewRepo.findAll()).thenReturn(allReviewsInModel);
+        mockMvc.perform(get("/reviews")).andExpect(model().attribute("reviewsModel", allReviewsInModel));
+    }
 }
